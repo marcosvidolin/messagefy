2cbc8e777e13ddb211fbad8602d10ecba8b1a0b9
marcosvidolin <marcosvidolin@gmail.com>
2019-08-03 20:36:53 -0300

    Unit tests

--- src/test/java/com/vidolima/messagefy/MessagefyTest.java
+++ src/test/java/com/vidolima/messagefy/MessagefyTest.java
@@ -1,5 +1,58 @@
 package com.vidolima.messagefy;
 
+import java.util.Arrays;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MessagefyTest {
 
+  @Test
+  public void test_build_withValidAttributes_mustPass() {
+    byte[] fakeAttachment = "fakeAttachmentFile".getBytes();
+    Messagefy messagefy = new Messagefy.Builder()
+        .from("messagefy@vidolima.com")
+        .senderName("Messagefy Library")
+        .to("vidolin@vidolima.com")
+        .ccList(Arrays.asList("test2@vidolima.com", "test3@vidolima.com"))
+        .subject("Messagefy!!")
+        .subjectCharset("utf-8")
+        .content("Hello Messagefy!")
+        .contentType("text/plain")
+        .attachmentName("AttFileName")
+        .attachment(fakeAttachment)
+        .attachmentMimeType("multipart/byteranges")
+        .build();
+
+    Assert.assertEquals(messagefy.getSender(), "messagefy@vidolima.com");
+    Assert.assertEquals(messagefy.getSenderName(), "Messagefy Library");
+    Assert.assertEquals(messagefy.getCcList().size(), 2);
+    Assert.assertEquals(messagefy.getToList().size(), 1);
+    Assert.assertEquals(messagefy.getToList().toArray()[0], "vidolin@vidolima.com");
+    Assert.assertEquals(messagefy.getSubject(), "Messagefy!!");
+    Assert.assertEquals(messagefy.getSubjectCharset(), "utf-8");
+    Assert.assertEquals(messagefy.getContent(), "Hello Messagefy!");
+    Assert.assertEquals(messagefy.getContentType(), "text/plain");
+    Assert.assertEquals(messagefy.getAttachmentName(), "AttFileName");
+    Assert.assertEquals(messagefy.getAttachment(), fakeAttachment);
+    Assert.assertEquals(messagefy.getAttachmentMimeType(), "multipart/byteranges");
+
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void test_build_withoutFrom_mustThrowException() {
+    new Messagefy.Builder()
+        .to("vidolima@vidolima.com")
+        .subject("Messagefy")
+        .content("Hello Messagefy!")
+        .build();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void test_build_withoutTo_mustThrowException() {
+    new Messagefy.Builder()
+        .from("vidolima@vidolima.com")
+        .subject("Messagefy")
+        .content("Hello Messagefy!")
+        .build();
+  }
 }
--- src/test/java/com/vidolima/messagefy/util/EmailUtilTest.java
+++ src/test/java/com/vidolima/messagefy/util/EmailUtilTest.java
@@ -1,16 +1,44 @@
 package com.vidolima.messagefy.util;
 
-import static org.junit.Assert.*;
-
+import com.vidolima.messagefy.exception.InvalidEmailException;
+import java.util.Arrays;
 import org.junit.Test;
 
 public class EmailUtilTest {
 
   @Test
-  public void isValid() {
+  public void test_isValid_withValidEmail_mustPass() {
+    EmailUtil.isValid("marcosvidolin@vidolima.com");
+  }
+
+  @Test(expected = InvalidEmailException.class)
+  public void test_isValid_withIncompleteEmail_mustThrowInvalidEmailException() {
+    EmailUtil.isValid("marcosvidolin@vidolima");
+  }
+
+  @Test(expected = InvalidEmailException.class)
+  public void test_isValid_withInvalidEmail_mustThrowInvalidEmailException() {
+    EmailUtil.isValid("marcosvidolin.com");
   }
 
   @Test
-  public void testIsValid() {
+  public void test_isValid_withValidEmailList_mustPass() {
+    EmailUtil.isValid(Arrays.asList("marcosvidolin@vidolima.com", "vidolin@messagefy.com"));
   }
+
+  @Test(expected = InvalidEmailException.class)
+  public void test_isValid_withInvalidEmailList_mustThrowInvalidEmailException() {
+    EmailUtil.isValid(Arrays.asList("marcos-vidolin@vidolima.com", "4vidolin@messagefy.com", "vidolin"));
+  }
+
+  @Test(expected = InvalidEmailException.class)
+  public void test_isValid_withNullEmailList_mustThrowInvalidEmailException() {
+    EmailUtil.isValid(Arrays.asList("marcos-vidolin@vidolima.com", "4vidolin@messagefy.com", null));
+  }
+
+  @Test(expected = InvalidEmailException.class)
+  public void test_isValid_withEmptyStringEmailList_mustThrowInvalidEmailException() {
+    EmailUtil.isValid(Arrays.asList("marcos-vidolin@vidolima.com", "4vidolin@messagefy.com", ""));
+  }
+
 }
